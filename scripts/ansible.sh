8bba6c5421ecdc202c65d8e6528fb86fcc36b52b
Andrew Finlay <andy.finlay@velocity42.com>
2016-05-11 21:41:47 +0100

    Update ansible.sh

--- scripts/ansible.sh
+++ scripts/ansible.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 # Install pip.
-apt -y install python-setuptools python-dev
+apt -y install python-setuptools python-dev build-essential libssl-dev libffi-dev
 easy_install pip
 
 # Install Ansible.
